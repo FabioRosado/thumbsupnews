c48458823681d5b14d3bb75f50c1f544b189bedf
FabioRosado <fabioglrosado@gmail.com>
2020-04-15 21:26:50 +0100

    Update script to both run scrapy, push to db and remove scrapy file

--- scripts/upload_backend.py
+++ scripts/upload_backend.py
@@ -1,13 +1,28 @@
+import os
+import subprocess
 import jsonlines
 import requests
 import time
 
 
+print("Starting to run scrapper to get todays feeds...")
+
+subprocess.call(["python", "../scrapper/run_spiders.py"])
+
+print("Done scrapping todays feeds. Starting to push them to the database...")
 
 with jsonlines.open('../scrapper/news.jl') as reader:
     for line in reader:
         print(line)
         r = requests.post('http://localhost:8000/headlines/',
-                          headers={'Authorization': "Token db8f2e530bf43f0ca9cab6c5013c9daf521d5278"},
+                          headers={'Authorization': f"Token {os.environ['DJANGO_TOKEN']}"},
                           data=line)
-        time.sleep(.5)
+
+print("Done uploading todays headlines to backend. Removing file...")
+
+try:
+    os.remove('../scrapper/news.jl')
+
+    print("Done. Headlines file removed successfully.")
+except Exception as error:
+    print(f"Ooops! There was an error trying to remove the file \nError: {error}")
