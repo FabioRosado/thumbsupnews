038be843b6cdac72063db15c4deae1ae59fc863b
FabioRosado <fabioglrosado@gmail.com>
2020-04-24 20:11:40 +0100

    Add close button to menu in mobile

--- frontend/components/card.js
+++ frontend/components/card.js
@@ -5,26 +5,26 @@ const Card = ({headline}) => {
   const description = headline.description.substring(0, 250)
 
   return (      
-  <article className="card">
-    <header>
-    <a href={headline.link}>
-    <span className="letter">{headline.title.charAt(0)}</span>
-      <p className="text-xs flex">
-        <img src={Icon} className="mr-2" width="20px" alt={headline.categories} /> {headline.categories}
-      </p>
-      <h1 className="mb-5 font-semibold text-lg">{headline.title}</h1>
-    </a>
-    </header>
-    <div className="h-full">
+    <article className="card">
+      <header>
       <a href={headline.link}>
-      <p className="text-sm">{description.replace(/\W$/, '')}... | Read More</p>
+      <span className="letter">{headline.title.charAt(0)}</span>
+        <p className="text-xs flex">
+          <img src={Icon} className="mr-2" width="20px" alt={headline.categories} /> {headline.categories}
+        </p>
+        <h1 className="mb-5 font-semibold text-lg">{headline.title}</h1>
       </a>
-    </div>
-    <footer className="text-xs flex justify-between">
-      <p className="date">{headline.date}</p>
-      <p className="source">{headline.source}</p>
-    </footer>
-  </article>
+      </header>
+      <div className="h-full">
+        <a href={headline.link}>
+        <p className="text-sm">{description.replace(/\W$/, '')}... | Read More</p>
+        </a>
+      </div>
+      <footer className="text-xs flex justify-between">
+        <p className="date">{headline.date}</p>
+        <p className="source">{headline.source}</p>
+      </footer>
+    </article>
   )};
 
 export default Card
--- frontend/components/header.js
+++ frontend/components/header.js
@@ -7,18 +7,19 @@ const Header = () => {
   const [show, setShow] = useState(false)
   
   return (
-  <header className="border-gray-300 border-bottom flex items-center justify-between p-5 min-w-full">
-   <Link href="/" >
-     <a><img src={Logo} alt="Thumbs Up News" width="250px" /></a>
-   </Link>
-  <button className="visible md:invisible p-5" onClick={() => setShow(!show)}><i className="gg-menu-right" /></button>
-   <nav className={show ? "navbar navbar-show" : "navbar navbar-hide"}>
-     <Link href="/"><a className="mr-5 nav-link">Home</a></Link>
-     <Link href="/about"><a className="mr-5 nav-link">About</a></Link>
-     <Link href="/contacts"><a className="mr-5 nav-link">Contacts</a></Link>
-     <Link href="/subscribe"><a className="primary-button">Subscribe</a></Link>
-   </nav>
-  </header>
+    <header className="border-gray-300 border-bottom flex items-center justify-between p-5 min-w-full">
+    <Link href="/" >
+      <a><img src={Logo} alt="Thumbs Up News" width="250px" /></a>
+    </Link>
+    <button aria-label="Toggle Navigation Menu" className="visible md:invisible p-5" onClick={() => setShow(!show)}><i className="gg-menu-right" /></button>
+    <nav className={show ? "navbar navbar-show" : "navbar navbar-hide"}>
+      <button aria-label="Close Nagivation Menu" onClick={() => setShow(False)} className="menu-button"><i className="gg-close" /></button>
+      <Link href="/"><a className="mr-5 nav-link">Home</a></Link>
+      <Link href="/about"><a className="mr-5 nav-link">About</a></Link>
+      <Link href="/contacts"><a className="mr-5 nav-link">Contacts</a></Link>
+      <Link href="/subscribe"><a className="primary-button">Subscribe</a></Link>
+    </nav>
+    </header>
 )};
 
 export default Header
