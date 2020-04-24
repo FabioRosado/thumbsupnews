006755d37565ab55e242f6ead7e886a8bdeed033
FabioRosado <fabioglrosado@gmail.com>
2020-04-24 19:01:50 +0100

    Move header to be response for mobile

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -1,20 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 import Link from "./link"
 import Logo from "../images/Logo.svg"
 
 
-const Header = () => (
+const Header = () => {
+  const [show, setShow] = useState(false)
+  
+  return (
   <header className="border-gray-300 border-bottom flex items-center justify-between p-5 min-w-full">
    <Link href="/" >
      <a><img src={Logo} alt="Thumbs Up News" width="250px" /></a>
    </Link>
-   <nav className="flex items-center">
+  <button className="visible md:invisible" onClick={() => setShow(!show)}><i className="gg-menu-right" /></button>
+   <nav className={show ? "navbar navbar-show" : "navbar navbar-hide"}>
      <Link href="/"><a className="mr-5 nav-link">Home</a></Link>
      <Link href="/about"><a className="mr-5 nav-link">About</a></Link>
      <Link href="/contacts"><a className="mr-5 nav-link">Contacts</a></Link>
      <Link href="/subscribe"><a className="primary-button">Subscribe</a></Link>
    </nav>
   </header>
-);
+)};
 
 export default Header
