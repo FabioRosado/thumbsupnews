b522d3d22858d4f5140aedbd36d78fc7f18396cf
FabioRosado <fabioglrosado@gmail.com>
2020-04-26 13:47:30 +0100

    Add category classifier to wallstreet scrapper

--- scrapper/scrapper/spiders/wallstreetjournal.py
+++ scrapper/scrapper/spiders/wallstreetjournal.py
@@ -7,20 +7,6 @@ from classifier import NewsHeadlineClassifier, CategoryClassifier
 from .helper import is_todays_article, transform_date, remove_html
 
 
-def get_categories(categories):
-    category = list(set(categories))[0]
-
-    if category == 'Future of Everything':
-        return 'Tech'
-
-    if 'Nguyen' in category:
-        return "Personal Technology"
-
-    if category == 'Half Full':
-        return "Entertainment"
-    return category
-
-
 class WallstreetJournalScrapper(XMLFeedSpider):
     name = 'wallstreet'
     start_urls = [
@@ -34,7 +20,21 @@ class WallstreetJournalScrapper(XMLFeedSpider):
     itertag = 'item'
 
     def __init__(self):
-        self.classifier = NewsHeadlineClassifier()
+        self.sentiment_classifier = NewsHeadlineClassifier()
+        self.category_classifier = CategoryClassifier()
+    
+    def get_categories(self, categories, title, classifier):
+        category = list(set(categories))[0]
+
+        if category == 'Future of Everything':
+            return 'Tech'
+
+        if 'Nguyen' in category:
+            return "Personal Technology"
+
+        if category == 'Half Full':
+            return "Entertainment"
+        return classifier.classify(title)
 
     def parse_node(self, response, node):
         sel = Selector(response)
@@ -48,7 +48,7 @@ class WallstreetJournalScrapper(XMLFeedSpider):
                 "link": node.xpath('link/text()').get().strip(),
                 "description": remove_html(node.xpath('description/text()').get()),
                 "date": transform_date(node.xpath('pubDate/text()').get()),
-                "categories": get_categories(sel.xpath('//wsj:articletype/text()').getall()),
+                "categories": self.get_categories(sel.xpath('//wsj:articletype/text()').getall(), title, self.category_classifier),
                 "source": "Wallstreet Journal",
-                "sentiment": self.classifier.classify(title)
+                "sentiment": self.sentiment_classifier.classify(title)
             }
