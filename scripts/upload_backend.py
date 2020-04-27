0eafa9f9fd649c3bc177c85924aba5b157e262f6
FabioRosado <fabioglrosado@gmail.com>
2020-04-27 11:57:38 +0100

    Update file paths to allow script being called from any folder

--- scripts/upload_backend.py
+++ scripts/upload_backend.py
@@ -4,15 +4,19 @@ import jsonlines
 import requests
 import time
 
+ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+NEWS_DATASET = os.path.join(ROOT, 'classifier', 'datasets', 'news_dataset.json')
+SCRAPPED_NEWS = os.path.join(ROOT, 'scrapper', 'news.jl')
 
 print("Adding scrapped news to the dataset...")
-with open('../classifier/datasets/new_classified_news.csv','a') as dataset:
-    with jsonlines.open('../scrapper/news.jl') as reader:
+with open(NEWS_DATASET,'a') as dataset:
+    with jsonlines.open(SCRAPPED_NEWS) as reader:
         for item in reader.iter(type=dict, skip_invalid=True):
-            dataset.write(f"{item['title']},{item['sentiment']}\n")
+            dataset.write(f"{item}\n")
 
 
-with jsonlines.open('../scrapper/news.jl') as reader:
+
+with jsonlines.open(SCRAPPED_NEWS) as reader:
     for line in reader:
         r = requests.post('http://localhost:8000/headlines/',
                         headers={'Authorization': f"Token {os.environ['DJANGO_TOKEN']}"},
@@ -21,7 +25,7 @@ with jsonlines.open('../scrapper/news.jl') as reader:
 print("Done uploading todays headlines to backend. Removing file...")
 
 try:
-    os.remove('../scrapper/news.jl')
+    os.remove(SCRAPPED_NEWS)
 
     print("Done. Headlines file removed successfully.")
 except Exception as error:
