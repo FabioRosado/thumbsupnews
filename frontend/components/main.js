d24acce048477c15ee2c6616892c3aeb5eeb40ae
FabioRosado <fabioglrosado@gmail.com>
2020-06-18 17:41:29 +0100

    Removed scroll event listener, added grid/list view, moved pagination to the end of page

--- frontend/components/main.js
+++ frontend/components/main.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import { animateScroll as scroll } from "react-scroll"
 
 import Link from "./link.js"
@@ -7,18 +7,9 @@ import Card from "./card"
 const Main = (props) => {
   const [data, setData] = useState(props.data)
   const pages = Array.from(Array(Math.round(data.count/24)).keys())
-  const [scrollPosition, setScrollPosition] = useState(0)
 
-  const handleScroll = () => {
-    const position = window.pageYOffset
-    setScrollPosition(position)
-  }
+  const [layout, setLayout] = useState('grid-view')
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
-    
-    return () => { window.removeEventListener('scroll', handleScroll)}
-  })
 
   const pagination = page => {
     fetch('/api/get-page', {method: 'POST', 'body': page})
@@ -56,24 +47,31 @@ const Main = (props) => {
             <li><Link href="/category/woman"><a className="sidebar-menu-item">Woman</a></Link></li>
           </ul>
         </div>
+        <section>
+          <div className="flex justify-end mt-8 mb-2 pr-12">
+          <button className="mr-6" aria-label="Change Layout to List" onClick={() => setLayout("list-view")}><i className="text-gray-700 gg-layout-list" /></button>
+          <button className="" aria-label="Change Layout to Grid" onClick={() => setLayout("grid-view")}><i className="text-gray-700 gg-layout-grid" /></button>
+          </div>
 
-        <div className="main-content">
-        {data.results.map(article => <Card headline={article} key={article.id} />)}
-          <button className={ scrollPosition > 150 ? 'move-top' : '' } aria-label="Move To Top of Page" onClick={() => scroll.scrollToTop()}><i className="gg-chevron-up mr-1" /></button>
-          <div className="pagination">
-          {pages.map(page => {
-              let url = ''
-              if (data.next) {
-                url = data.next.replace(/page=\d/gi, `page=${page+1}`)
-              } 
+          <div className={layout === "grid-view" ? `main-content grid-view` : `main-content list-view`}>
+          {data.results.map(article => <Card headline={article} key={article.id} />)}
+            <button className="move-top" aria-label="Move To Top of Page" onClick={() => scroll.scrollToTop()}><i className="gg-chevron-up mr-1" /></button>
+          </div>
+          <div className="pagination flex justify-center mb-8">
+            {pages.map(page => {
+                let url = ''
+                if (data.next) {
+                  url = data.next.replace(/page=\d/gi, `page=${page+1}`)
+                } 
 
-              if (data.previous && !data.next) {
-                url = data.previous.replace(/page=\d/gi, `page=${page+1}`)
-              }
-              return <button key={page} aria-label={`Move to page ${page+1}, will scroll to top`} className="link" onClick={() => pagination(url)}>{page+1}</button>
-          })}
-        </div>
-        </div>
+                if (data.previous && !data.next) {
+                  url = data.previous.replace(/page=\d/gi, `page=${page+1}`)
+                }
+                return <button key={page} aria-label={`Move to page ${page+1}, will scroll to top`} className="link" onClick={() => pagination(url)}>{page+1}</button>
+            })}
+          </div>
+        
+        </section>
       </div>
  
 )}
